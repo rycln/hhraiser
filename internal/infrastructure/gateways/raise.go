3d136139b8ce2ee01f54720981014dec3f9903be
Ruslan Khalikov <rycln1@gmail.com>
2026-04-18 20:12:11 +0300

    feat: add debug logging

--- internal/infrastructure/gateways/raise.go
+++ internal/infrastructure/gateways/raise.go
@@ -15,7 +15,7 @@ import (
 const raiseEndpoint = "/applicant/resumes/touch"
 
 func (g *Gateway) Raise(ctx context.Context, resume *domain.Resume, session *domain.Session) error {
-	slog.DebugContext(ctx, "attempting raise", "url", g.baseURL)
+	slog.DebugContext(ctx, "attempting raise")
 
 	fullURL, err := url.JoinPath(g.baseURL, raiseEndpoint)
 	if err != nil {
@@ -48,10 +48,25 @@ func (g *Gateway) Raise(ctx context.Context, resume *domain.Resume, session *dom
 	case http.StatusConflict:
 		return domain.ErrRaiseTooEarly
 	default:
-		slog.ErrorContext(ctx, "request failed with status",
+		logAttrs := []any{
 			"status_code", resp.StatusCode,
-			"status_text", http.StatusText(resp.StatusCode))
-		return domain.ErrInvalidSession
+			"status_text", http.StatusText(resp.StatusCode),
+		}
+
+		if slog.Default().Enabled(ctx, slog.LevelDebug) {
+			bodyBytes, readErr := io.ReadAll(io.LimitReader(resp.Body, 5120))
+			if readErr != nil {
+				logAttrs = append(logAttrs, "body_read_error", readErr)
+			} else {
+				logAttrs = append(logAttrs, "body", string(bodyBytes))
+			}
+			bodyString := string(bodyBytes)
+
+			logAttrs = append(logAttrs, "headers", resp.Header, "body", bodyString)
+		}
+
+		slog.ErrorContext(ctx, "raise request failed", logAttrs...)
+		return domain.ErrRaiseUnexpectedResponse
 	}
 }
 
